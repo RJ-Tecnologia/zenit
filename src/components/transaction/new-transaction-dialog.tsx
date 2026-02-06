33e5319539679835f8d6fe7a02e10de3339b1713
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-02-06 20:16:49 -0300

    feature: adicionando controle do formulário

--- src/components/transaction/new-transaction-dialog.tsx
+++ src/components/transaction/new-transaction-dialog.tsx
@@ -1,7 +1,10 @@
 'use client'
 
+import { zodResolver } from '@hookform/resolvers/zod'
 import { Plus } from 'lucide-react'
 import { useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { z } from 'zod/v3'
 import { Button } from '../ui/button'
 import {
   Dialog,
@@ -11,8 +14,8 @@ import {
   DialogTitle,
   DialogTrigger
 } from '../ui/dialog'
+import { Field, FieldError, FieldGroup, FieldLabel } from '../ui/field'
 import { Input } from '../ui/input'
-import { Label } from '../ui/label'
 import {
   Select,
   SelectContent,
@@ -21,14 +24,39 @@ import {
   SelectValue
 } from '../ui/select'
 
+const newTransactionformSchema = z.object({
+  title: z.string().min(1, { message: 'O campo é obrigatório' }),
+  type: z.enum(['INCOME', 'OUTCOME']),
+  amount: z.coerce.number().gt(0, { message: 'O valor deve ser maior que 0' }),
+  description: z.string().optional(),
+  categoryId: z.string({ message: 'Campo obrigatório' }).uuid(),
+  date: z.date({ message: 'Campo obrigatório' })
+})
+
+type NewTransactionformSchema = z.infer<typeof newTransactionformSchema>
+
 export function NewTransactionDialog() {
   const [open, setOpen] = useState(false)
+
+  const form = useForm<NewTransactionformSchema>({
+    resolver: zodResolver(newTransactionformSchema),
+    defaultValues: {
+      title: '',
+      description: '',
+      type: 'INCOME',
+      amount: 0
+    }
+  })
+
+  function handleSubmit(data: NewTransactionformSchema) {
+    console.log(data)
+  }
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
         <Button className="gap-2">
-          <Plus className="h-4 w-4" />
+          <Plus className="size-4" />
           Nova Transação
         </Button>
       </DialogTrigger>
@@ -39,69 +67,189 @@ export function NewTransactionDialog() {
             Adicione uma nova transação ao seu registro financeiro
           </DialogDescription>
         </DialogHeader>
-        <form className="space-y-4 pt-4">
-          <div className="space-y-2">
-            <Label htmlFor="title">Título</Label>
-            <Input id="title" placeholder="Ex: Compras do mês" />
-          </div>
+        <form className="pt-4" onSubmit={form.handleSubmit(handleSubmit)}>
+          <FieldGroup className="gap-2">
+            <Controller
+              control={form.control}
+              name="title"
+              render={({ field, fieldState }) => (
+                <Field>
+                  <div className="space-y-2">
+                    <FieldLabel htmlFor="title">Título</FieldLabel>
+                    <Input
+                      {...field}
+                      id="title"
+                      placeholder="Ex: Compras do mês"
+                    />
+                  </div>
 
-          <div className="space-y-2">
-            <Label htmlFor="type">Tipo</Label>
-            <Select>
-              <SelectTrigger id="type">
-                <SelectValue placeholder="Selecione o tipo" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="INCOME">Entrada</SelectItem>
-                <SelectItem value="OUTCOME">Saída</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
+                  {fieldState.invalid && (
+                    <FieldError
+                      errors={[fieldState.error]}
+                      className="text-xs"
+                    />
+                  )}
+                </Field>
+              )}
+            />
 
-          <div className="space-y-2">
-            <Label htmlFor="amount">Valor</Label>
-            <Input id="amount" type="number" step="0.01" placeholder="0,00" />
-          </div>
+            <Controller
+              control={form.control}
+              name="type"
+              render={({ field, fieldState }) => (
+                <Field>
+                  <div className="space-y-2">
+                    <FieldLabel htmlFor="type">Tipo</FieldLabel>
+                    <Select onValueChange={field.onChange} value={field.value}>
+                      <SelectTrigger id="type">
+                        <SelectValue placeholder="Selecione o tipo" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="INCOME">Entrada</SelectItem>
+                        <SelectItem value="OUTCOME">Saída</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
 
-          <div className="space-y-2">
-            <Label htmlFor="category">Categoria</Label>
-            <Select>
-              <SelectTrigger id="category">
-                <SelectValue placeholder="Selecione a categoria" />
-              </SelectTrigger>
-              <SelectContent>
-                <SelectItem value="trabalho">Trabalho</SelectItem>
-                <SelectItem value="mercado">Mercado</SelectItem>
-                <SelectItem value="investimento">Investimento</SelectItem>
-                <SelectItem value="alimentacao">Alimentação</SelectItem>
-                <SelectItem value="outros">Outros</SelectItem>
-              </SelectContent>
-            </Select>
-          </div>
+                  {fieldState.invalid && (
+                    <FieldError
+                      errors={[fieldState.error]}
+                      className="text-xs"
+                    />
+                  )}
+                </Field>
+              )}
+            />
 
-          <div className="space-y-2">
-            <Label htmlFor="date">Data</Label>
-            <Input id="date" type="datetime-local" />
-          </div>
+            <Controller
+              control={form.control}
+              name="amount"
+              render={({ field, fieldState }) => (
+                <Field>
+                  <div className="space-y-2">
+                    <FieldLabel htmlFor="amount">Valor</FieldLabel>
+                    <Input
+                      {...field}
+                      id="amount"
+                      type="number"
+                      step="0.01"
+                      placeholder="0,00"
+                    />
+                  </div>
 
-          <div className="space-y-2">
-            <Label htmlFor="description">Descrição (opcional)</Label>
-            <Input id="description" placeholder="Detalhes adicionais" />
-          </div>
+                  {fieldState.invalid && (
+                    <FieldError
+                      errors={[fieldState.error]}
+                      className="text-xs"
+                    />
+                  )}
+                </Field>
+              )}
+            />
 
-          <div className="flex gap-3 pt-4">
-            <Button
-              type="button"
-              variant="outline"
-              className="flex-1"
-              onClick={() => setOpen(false)}
-            >
-              Cancelar
-            </Button>
-            <Button type="submit" className="flex-1">
-              Adicionar
-            </Button>
-          </div>
+            <Controller
+              control={form.control}
+              name="categoryId"
+              render={({ field, fieldState }) => (
+                <Field>
+                  <div className="space-y-2">
+                    <FieldLabel htmlFor="category">Categoria</FieldLabel>
+                    <Select onValueChange={field.onChange} value={field.value}>
+                      <SelectTrigger id="category">
+                        <SelectValue placeholder="Selecione a categoria" />
+                      </SelectTrigger>
+                      <SelectContent>
+                        <SelectItem value="trabalho">Trabalho</SelectItem>
+                        <SelectItem value="mercado">Mercado</SelectItem>
+                        <SelectItem value="investimento">
+                          Investimento
+                        </SelectItem>
+                        <SelectItem value="alimentacao">Alimentação</SelectItem>
+                        <SelectItem value="outros">Outros</SelectItem>
+                      </SelectContent>
+                    </Select>
+                  </div>
+
+                  {fieldState.invalid && (
+                    <FieldError
+                      errors={[fieldState.error]}
+                      className="text-xs"
+                    />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              control={form.control}
+              name="date"
+              render={({ field, fieldState }) => (
+                <Field>
+                  <div className="space-y-2">
+                    <FieldLabel htmlFor="date">Data</FieldLabel>
+                    <Input
+                      {...field}
+                      id="date"
+                      type="datetime-local"
+                      value={
+                        field.value instanceof Date
+                          ? field.value.toISOString().slice(0, 16)
+                          : ''
+                      }
+                      onChange={(e) => field.onChange(new Date(e.target.value))}
+                    />
+                  </div>
+
+                  {fieldState.invalid && (
+                    <FieldError
+                      errors={[fieldState.error]}
+                      className="text-xs"
+                    />
+                  )}
+                </Field>
+              )}
+            />
+
+            <Controller
+              control={form.control}
+              name="description"
+              render={({ field, fieldState }) => (
+                <Field>
+                  <div className="space-y-2">
+                    <FieldLabel htmlFor="description">
+                      Descrição (opcional)
+                    </FieldLabel>
+                    <Input
+                      {...field}
+                      id="description"
+                      placeholder="Detalhes adicionais"
+                    />
+                  </div>
+
+                  {fieldState.invalid && (
+                    <FieldError
+                      errors={[fieldState.error]}
+                      className="text-xs"
+                    />
+                  )}
+                </Field>
+              )}
+            />
+
+            <div className="flex gap-3 pt-4">
+              <Button
+                type="button"
+                variant="outline"
+                className="flex-1"
+                onClick={() => setOpen(false)}
+              >
+                Cancelar
+              </Button>
+              <Button type="submit" className="flex-1">
+                Adicionar
+              </Button>
+            </div>
+          </FieldGroup>
         </form>
       </DialogContent>
     </Dialog>
