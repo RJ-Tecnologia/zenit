eb099ac64fb0a89f89d3bdcd79762e567083b7a8
Tiago Lopes <tiagoltavares2002@gmail.com>
2026-03-05 17:58:00 -0300

    feature: melhorando o input de data
    - Utilizando o componente calendar do Shadcn
    - Definindo o locale como pt-BR

--- src/features/transactions/components/transaction-form-fields.tsx
+++ src/features/transactions/components/transaction-form-fields.tsx
@@ -1,10 +1,20 @@
+import { format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
+import { CalendarIcon } from 'lucide-react'
 import { type Control, Controller } from 'react-hook-form'
+import { Button } from '@/components/ui/button'
+import { Calendar } from '@/components/ui/calendar'
 import {
   Field,
   FieldError as FieldErrorComponent,
   FieldLabel
 } from '@/components/ui/field'
 import { Input } from '@/components/ui/input'
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from '@/components/ui/popover'
 import {
   Select,
   SelectContent,
@@ -13,6 +23,7 @@ import {
   SelectValue
 } from '@/components/ui/select'
 import type { Category } from '@/generated/prisma/client'
+import { cn } from '@/lib/utils'
 import type { NewTransactionformSchema } from '../schemas/new-transaction-schema'
 
 interface FormFieldProps {
@@ -152,18 +163,32 @@ export function DateField({ control }: FormFieldProps) {
       render={({ field, fieldState }) => (
         <Field>
           <div className="space-y-2">
-            <FieldLabel htmlFor="date">Data</FieldLabel>
-            <Input
-              {...field}
-              id="date"
-              type="datetime-local"
-              value={
-                field.value instanceof Date
-                  ? field.value.toISOString().slice(0, 16)
-                  : ''
-              }
-              onChange={(e) => field.onChange(new Date(e.target.value))}
-            />
+            <FieldLabel>Data</FieldLabel>
+            <Popover>
+              <PopoverTrigger asChild>
+                <Button
+                  variant="outline"
+                  className={cn(
+                    'w-full justify-start text-left font-normal',
+                    !field.value && 'text-muted-foreground'
+                  )}
+                >
+                  <CalendarIcon className="mr-2 size-4" />
+                  {field.value
+                    ? format(field.value, 'dd/MM/yyyy', { locale: ptBR })
+                    : 'Selecione uma data'}
+                </Button>
+              </PopoverTrigger>
+              <PopoverContent className="w-auto p-0" align="start">
+                <Calendar
+                  mode="single"
+                  selected={field.value}
+                  onSelect={field.onChange}
+                  locale={ptBR}
+                  autoFocus
+                />
+              </PopoverContent>
+            </Popover>
           </div>
 
           {fieldState.invalid && (
